608b1a6afe1cf094b6d975c574b60c17cc86ac39
David Mitchell <davidbmitchell77@outlook.com>
2021-12-28 15:03:14 -0800

    lwc:dom="manual"

--- force-app/main/default/lwc/helloQuerySelectorDemo/helloQuerySelectorDemo.js
+++ force-app/main/default/lwc/helloQuerySelectorDemo/helloQuerySelectorDemo.js
@@ -2,16 +2,23 @@ import { LightningElement } from 'lwc';
 
 export default class HelloQuerySelectorDemo extends LightningElement
 {
+    isDisplayed = false;
     users = ["Gene", "Paul", "Ace", "Peter"];
  
     fetchDetailHandler()
     {
         const h1 = this.template.querySelector("h1");
         h1.style.border = "1px solid maroon";
-      //console.log(h1.innerText);
+
         const userElements = this.template.querySelectorAll(".name");
         Array.from(userElements).forEach(element => {
-            console.log(element.innerText);
+            element.setAttribute("title", element.innerText);
         });
+
+      //lwc:dom="manual" example  
+        const childElement = this.template.querySelector(".child");
+        childElement.innerHTML = "<p>Hey!&nbsp;&nbsp;I am a child element.</p>";
+
+        this.isDisplayed = true;
     }
 }
