68415fd89df532951b13d64723ccbde112901051
David Mitchell <davidbmitchell77@outlook.com>
2021-12-27 21:36:58 -0800

    cleanup

--- force-app/main/default/lwc/quizApp/quizApp.js
+++ force-app/main/default/lwc/quizApp/quizApp.js
@@ -1,4 +1,3 @@
-import EmailPreferencesStayInTouchReminder from '@salesforce/schema/User.EmailPreferencesStayInTouchReminder';
 import { LightningElement } from 'lwc';
 
 export default class QuizApp extends LightningElement
