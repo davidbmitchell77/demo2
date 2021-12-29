6ffe23b1a495da68be0594cc9c887e254e65b812
David Mitchell <davidbmitchell77@outlook.com>
2021-12-28 16:40:28 -0800

    footer2

--- force-app/main/default/lwc/helloQuerySelectorDemo/helloQuerySelectorDemo.js
+++ force-app/main/default/lwc/helloQuerySelectorDemo/helloQuerySelectorDemo.js
@@ -1,24 +1,42 @@
-import { LightningElement } from 'lwc';
+import { LightningElement, track } from 'lwc';
 
 export default class HelloQuerySelectorDemo extends LightningElement
 {
-    isDisplayed = false;
+    @track isDisplayed = false;
     users = ["Gene", "Paul", "Ace", "Peter"];
  
-    fetchDetailHandler()
+    showDetailsHandler()
     {
         const h1 = this.template.querySelector("h1");
         h1.style.border = "1px solid maroon";
+
+      //lwc:dom="manual" example 1
+        const headerElement = this.template.querySelector(".header");
+        headerElement.innerHTML = "<p>Hey!&nbsp;&nbsp;I am a header.</p>";
 
         const userElements = this.template.querySelectorAll(".name");
         Array.from(userElements).forEach(element => {
             element.setAttribute("title", element.innerText);
         });
 
-      //lwc:dom="manual" example  
-        const childElement = this.template.querySelector(".child");
-        childElement.innerHTML = "<p>Hey!&nbsp;&nbsp;I am a child element.</p>";
+      //lwc:dom="manual" example 2
+        const footerElement = this.template.querySelector(".footer");
+        footerElement.innerHTML = "<p>Hey!&nbsp;&nbsp;I am a footer.</p>";
 
         this.isDisplayed = true;
+    }
+
+    hideDetailsHandler()
+    {
+        const h1 = this.template.querySelector("h1");
+        h1.style.border = "none";
+
+        const headerElement = this.template.querySelector(".header");
+        headerElement.innerHTML = "";
+  
+        const footerElement = this.template.querySelector(".footer");
+        footerElement.innerHTML = "";
+ 
+        this.isDisplayed = false;
     }
 }
