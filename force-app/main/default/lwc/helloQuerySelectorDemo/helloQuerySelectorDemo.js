4af5d87d579585499ad4cf83568723c97a05e427
David Mitchell <davidbmitchell77@outlook.com>
2021-12-29 12:55:32 -0800

    #FF2400

--- force-app/main/default/lwc/helloQuerySelectorDemo/helloQuerySelectorDemo.js
+++ force-app/main/default/lwc/helloQuerySelectorDemo/helloQuerySelectorDemo.js
@@ -3,16 +3,17 @@ import { LightningElement } from 'lwc';
 export default class HelloQuerySelectorDemo extends LightningElement
 {
     isDisplayed = false;
-    users = ["Gene", "Paul", "Ace", "Peter"];
+    users = ["Gene Simmons", "Paul Stanley", "Ace Frehley", "Peter Criss"];
  
     showDetailsHandler()
     {
         const h1 = this.template.querySelector("h1");
-        h1.style.border = "2px solid maroon";
+        h1.style.color = "maroon";
+        h1.style.fontWeight = "bold";
 
       //lwc:dom="manual" example
         const headerElement = this.template.querySelector(".header");
-        headerElement.innerHTML = "<p>Hey!&nbsp;&nbsp;I am a header.</p>";
+        headerElement.innerHTML = '<p style="color:#ff2400; font-weight:bold;">KISS ROCKS!!!</p>';
 
         const userElements = this.template.querySelectorAll(".name");
         Array.from(userElements).forEach(element => {
@@ -25,7 +26,8 @@ export default class HelloQuerySelectorDemo extends LightningElement
     hideDetailsHandler()
     {
         const h1 = this.template.querySelector("h1");
-        h1.style.border = "none";
+        h1.style.color = "black";
+        h1.style.fontWeight = "normal";
 
         const headerElement = this.template.querySelector(".header");
         headerElement.innerHTML = "";
