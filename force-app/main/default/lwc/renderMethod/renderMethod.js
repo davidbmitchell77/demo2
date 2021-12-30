06b26e6d7659c14ff9604b104912898376349502
David Mitchell <davidbmitchell77@outlook.com>
2021-12-30 03:29:04 -0800

    button handlers

---
+++ force-app/main/default/lwc/renderMethod/renderMethod.js
@@ -0,0 +1,29 @@
+import { LightningElement } from 'lwc';
+import loginTemplate from './loginTemplate.html';
+import signupTemplate from './signupTemplate.html';
+import renderTemplate from './renderMethod.html';
+
+export default class RenderMethod extends LightningElement
+{
+    selectedButton = "";
+
+    render()
+    {
+        let result = renderTemplate;
+        if (this.selectedButton.toLowerCase() === "signup") {
+            result = signupTemplate;
+        }
+        if (this.selectedButton.toLowerCase() === "login") {
+            result = loginTemplate;
+        }
+        return result;
+    }
+
+    handleClick(event) {
+        this.selectedButton = event.target.label;
+    }
+
+    submitHandler(event) {
+        console.log(`${event.target.label} successfully.`);
+    }
+}
