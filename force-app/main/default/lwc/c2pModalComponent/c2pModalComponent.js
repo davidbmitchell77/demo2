9df342e2a8c1023499c1c33761e1f63efacb46d6
David Mitchell <davidbmitchell77@outlook.com>
2021-12-30 18:44:21 -0800

    event with data

--- force-app/main/default/lwc/c2pModalComponent/c2pModalComponent.js
+++ force-app/main/default/lwc/c2pModalComponent/c2pModalComponent.js
@@ -3,6 +3,6 @@ import { LightningElement } from 'lwc';
 export default class C2pModalComponent extends LightningElement
 {
     closeHandler() {
-        this.dispatchEvent(new CustomEvent("close"));
+        this.dispatchEvent(new CustomEvent("close", { detail: { msg: "Modal closed successfully!" }}));
     }
 }
--- force-app/main/default/lwc/c2pParentComponent/c2pParentComponent.js
+++ force-app/main/default/lwc/c2pParentComponent/c2pParentComponent.js
@@ -2,13 +2,16 @@ import { LightningElement } from 'lwc';
 
 export default class C2pParentComponent extends LightningElement
 {
+    msg = "";
     showModal = false;
 
     clickHandler() {
         this.showModal = true;
+        this.msg = "";
     }
 
-    closeHandler() {
+    closeHandler(event) {
+        this.msg = event.detail.msg;
         this.showModal = false;
     }
 }
