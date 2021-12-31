6908b223ade778d78c1072dd2f84fae12a2097d6
David Mitchell <davidbmitchell77@outlook.com>
2021-12-30 19:36:10 -0800

    bubbles: false

--- force-app/main/default/lwc/c2pModalComponent/c2pModalComponent.js
+++ force-app/main/default/lwc/c2pModalComponent/c2pModalComponent.js
@@ -3,6 +3,6 @@ import { LightningElement } from 'lwc';
 export default class C2pModalComponent extends LightningElement
 {
     closeHandler() {
-        this.dispatchEvent(new CustomEvent("close", { detail: { msg: "Modal closed successfully!" }}));
+        this.dispatchEvent(new CustomEvent("close", { bubbles: false, detail: { msg: "Modal closed successfully!" } }));
     }
 }
