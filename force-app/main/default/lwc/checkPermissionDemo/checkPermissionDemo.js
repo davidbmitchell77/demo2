66119005c23e2f32852f3a20ef7baa72363c6358
David Mitchell <davidbmitchell77@outlook.com>
2022-01-04 19:46:46 -0800

    array push

---
+++ force-app/main/default/lwc/checkPermissionDemo/checkPermissionDemo.js
@@ -0,0 +1,26 @@
+import { LightningElement } from "lwc";
+import HasViewAllData from "@salesforce/userPermission/ViewAllData";
+import ModifyAllData from "@salesforce/userPermission/ModifyAllData";
+import ManageEncyptionKeys from "@salesforce/userPermission/ManageEncyptionKeys";
+import AssignPermissionSets from "@salesforce/userPermission/AssignPermissionSets";
+
+export default class CheckPermissionDemo extends LightningElement
+{
+    values = [ true ];
+
+    get userPermissions()
+    {
+        let results = new Array();
+
+        results.push({ label: "View All Data", value: HasViewAllData });
+        results.push({ label: 'Modify All Data', value: ModifyAllData });
+        results.push({ label: 'REST API Access', value: ManageEncyptionKeys });
+        results.push({ label: 'Assign Permission Sets', value: AssignPermissionSets });
+
+        return results;
+    }
+
+    get hasViewAllData() {
+        return hasViewAllData;
+    }
+}
