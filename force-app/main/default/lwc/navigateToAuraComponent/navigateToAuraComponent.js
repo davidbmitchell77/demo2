30bf6229c0934b11071a6c3b141346592424fd70
David Mitchell <davidbmitchell77@outlook.com>
2022-01-06 21:15:27 -0800

    navigateToAuraComponent

---
+++ force-app/main/default/lwc/navigateToAuraComponent/navigateToAuraComponent.js
@@ -0,0 +1,39 @@
+import { LightningElement } from 'lwc';
+import { NavigationMixin  } from 'lightning/navigation';
+
+export default class NavigateToAuraComponent extends NavigationMixin(LightningElement)
+{
+    clickHandler(event)
+    {
+        let pageType = "";
+        let componentName = "";
+        let id = "";
+     
+        let buttonLabel = event.target.label;
+
+        if (buttonLabel === "Aura Component")
+        {
+            pageType = "standard_component";
+            componentName = "c__AuraNavigation";
+            id = "8af7d5c9-76e7-4d48-afed-6032b0e64cb3";
+ 
+            this.navigation(pageType, componentName, id);
+        }
+    }
+
+    navigation(pageType, componentName, id)
+    {
+        let attributes = {};
+        attributes.componentName = componentName;
+        let pageRef = {};
+
+        let state = {};
+        state.c__id = id;
+
+        pageRef.type = pageType;
+        pageRef.attributes = attributes;
+        pageRef.state = state;
+        
+        this[NavigationMixin.Navigate](pageRef);
+    }
+}
