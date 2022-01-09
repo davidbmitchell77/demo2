c123f9545fda3ed2d6f037004028939ea776957b
David Mitchell <davidbmitchell77@outlook.com>
2022-01-09 11:45:45 -0800

    typo

--- force-app/main/default/lwc/recordEditCustom/recordEditCustom.js
+++ force-app/main/default/lwc/recordEditCustom/recordEditCustom.js
@@ -1,3 +1,47 @@
-import { LightningElement } from 'lwc';
+import { LightningElement, api } from 'lwc';
+import { ShowToastEvent        } from 'lightning/platformShowToastEvent'
 
-export default class RecordEditCustom extends LightningElement {}
+import Account from '@salesforce/schema/Account';
+
+export default class RecordEditCustom extends LightningElement
+{
+    @api objectApiName = Account;
+    inputValue = null;
+
+    keyupHandler(event)
+    {
+        this.inputValue = event.target.value;
+    }
+
+    submitHandler(event)
+    {
+        event.prevetDefault();
+
+        let lightningInput = this.template.querySelector("lightning-input");
+        if (lightningInput)
+        {
+            if (!lightningInput.value.toLowerCase().contains("LLC")) {
+                lightningInput.setCustomValidity(`Account name must include "LLC."`);
+            }
+        }
+        else
+        {
+            lightningInput.setCustomValidity(`Account name must include "LLC."`);
+            let fields = event.detail.fields;
+            fields.Name = this.inputValue;
+            this.template.querySelector("lightning-record-edit-form").submit(fields);
+        }
+        lightningInput.reportValidity();
+    }
+
+    successHandler(event)
+    {
+        successMsg = new ShowToastEvent
+        ({
+            title: "Account created.",
+            message: `Record Id: ${event.target.id}`,
+            variant: "success"
+        })
+        this.dispatchEvent(successMsg);
+    }
+}
