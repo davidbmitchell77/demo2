16748861eb2171d23b7d23e6f3fbfceca6d8143d
David Mitchell <davidbmitchell77@outlook.com>
2022-01-10 16:10:56 -0800

    <p>Id: {userDetailsProperty.data.fields.Id.value}</p>

--- force-app/main/default/lwc/wireDemoUserDetailAsAProperty/wireDemoUserDetailAsAProperty.js
+++ force-app/main/default/lwc/wireDemoUserDetailAsAProperty/wireDemoUserDetailAsAProperty.js
@@ -1,24 +1,18 @@
 import { LightningElement, wire } from 'lwc';
 import { getRecord              } from 'lightning/uiRecordApi';
 
-import Id from '@salesforce/user/Id';
+import Id    from '@salesforce/user/Id';
+import Name  from '@salesforce/schema/User.Name';
+import Title from '@salesforce/schema/User.Title';
+import Email from '@salesforce/schema/User.Email';
+import Phone from '@salesforce/schema/User.Phone';
+
+const fields = [Id, Name, Title, Email, Phone];
 
 export default class WireDemoUserDetail extends LightningElement
 {
     userId = Id;
 
-    @wire
-    (
-        getRecord,
-        {
-            recordId: Id,
-            fields:
-            [
-                "User.Name",
-                "User.Title",
-                "User.Email",
-                "User.Phone"
-            ]
-        }
-    ) userDetailsProperty;
+    @wire(getRecord, { recordId: Id, fields })
+    userDetailsProperty;
 }
