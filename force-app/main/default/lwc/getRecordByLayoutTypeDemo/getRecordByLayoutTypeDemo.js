3b1eb0c32b7b2f5e889fb313ca939a27d32c195b
David Mitchell <davidbmitchell77@outlook.com>
2022-01-11 18:20:19 -0800

    ...

--- force-app/main/default/lwc/getRecordByLayoutTypeDemo/getRecordByLayoutTypeDemo.js
+++ force-app/main/default/lwc/getRecordByLayoutTypeDemo/getRecordByLayoutTypeDemo.js
@@ -1,10 +1,6 @@
 import { LightningElement, api, wire } from 'lwc';
 import { getRecord } from 'lightning/uiRecordApi';
 
-import Name          from '@salesforce/schema/Lead.Name';
-import City          from '@salesforce/schema/Lead.City';
-import State         from '@salesforce/schema/Lead.State';
-import PostalCode    from '@salesforce/schema/Lead.PostalCode';
 import AnnualRevenue from '@salesforce/schema/Lead.AnnualRevenue';
 
 export default class GetRecordByLayoutTypeDemo extends LightningElement
