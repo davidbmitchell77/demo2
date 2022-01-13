b9598e5232a753da9b30909f62e8482e1a9d6d27
David Mitchell <davidbmitchell77@outlook.com>
2022-01-13 04:45:04 -0800

    Update deleteRecordDemo.js

--- force-app/main/default/lwc/deleteRecordDemo/deleteRecordDemo.js
+++ force-app/main/default/lwc/deleteRecordDemo/deleteRecordDemo.js
@@ -1,24 +1,41 @@
-import { LightningElement } from 'lwc';
-import {deleteRecord} from 'lightning/uiRecordApi'
-import { ShowToastEvent } from 'lightning/platformShowToastEvent';
-export default class DeleteRecordDemo extends LightningElement {
-    recordId
-    changeHandler(event){
-       this.recordId = event.target.value
+import { LightningElement } from "lwc";
+import { ShowToastEvent   } from "lightning/platformShowToastEvent";
+import { deleteRecord     } from "lightning/uiRecordApi";
+
+export default class DeleteRecordDemo extends LightningElement
+{
+    recordId;
+
+    changeHandler(event) {
+       this.recordId = event.target.value;
     }
-    deleteHandler(){
-        deleteRecord(this.recordId).then(()=>{
-            this.showToast("Sucess!!", "Deleted Successfully!!", 'success')
-        }).catch(error=>{
-            console.error(error)
-            this.showToast("Error!!", "Error Occurred!!", 'error')
-        })
+
+    deleteHandler()
+    {
+        deleteRecord(this.recordId).then
+        (
+            () => {
+                this.showToast("Sucess!!", "Deleted Successfully!!", "success");
+            }
+        ).catch
+        (
+            error => {
+                this.showToast("Error!!", "Error Occurred!!", "error");
+                console.error(error);
+            }
+        )
     }
-    showToast(title, message, variant){
-        this.dispatchEvent(new ShowToastEvent({
-            title,
-            message,
-            variant
-        }))
+
+    showToast(title, message, variant)
+    {
+        this.dispatchEvent
+        (
+            new ShowToastEvent
+            ({
+                title: title,
+                message: message,
+                variant: (variant || "info")
+            })
+        );
     }
 }
