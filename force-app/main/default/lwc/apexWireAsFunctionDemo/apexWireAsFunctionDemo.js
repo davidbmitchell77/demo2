8e06946dc69b28577ed11637c82654d4694e7ac1
David Mitchell <davidbmitchell77@outlook.com>
2022-01-13 17:56:37 -0800

    ...

---
+++ force-app/main/default/lwc/apexWireAsFunctionDemo/apexWireAsFunctionDemo.js
@@ -0,0 +1,30 @@
+import { LightningElement, wire } from "lwc";
+
+import getAccountList from "@salesforce/apex/AccountController.getAccountList";
+
+export default class ApexWireDemo extends LightningElement
+{
+    accounts;
+
+    @wire(getAccountList)
+    getAccounts({ data, error })
+    {
+        if (data)
+        {
+            this.accounts = data.map
+            (
+                account =>
+                {
+                    let newType = account.Type;
+                    if (newType === "Customer - Channel") { newType = "Channel"; }
+                    if (newType === "Customer - Direct" ) { newType = "Direct";  }
+                    return { ...account, Type: newType };
+                }
+            );
+        }
+
+        if (error) {
+            console.error(error);
+        }
+    }
+}
