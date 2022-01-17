0e7bd31b52544d5e89d33757b05f9e9cdddf84a5
David Mitchell <davidbmitchell77@outlook.com>
2022-01-17 02:21:26 -0800

    popupCenter

--- force-app/main/default/lwc/googleBooks/googleBooks.js
+++ force-app/main/default/lwc/googleBooks/googleBooks.js
@@ -37,6 +37,23 @@ export default class GoogleBooks extends LightningElement
         );
     }
 
+    clickHandler(event)
+    {
+        let name = event.target.name;
+
+        if (name.includes("https://"))
+        {
+            this.popupCenter
+            (
+                name,
+                "_blank",
+                1300,
+                850
+            );
+            return false;
+        }
+    }
+
     log(message)
     {
         let messageType = Object.prototype.toString.call(message);
@@ -45,5 +62,35 @@ export default class GoogleBooks extends LightningElement
         if (messageType === "[object Array]" ) { console.info(message);  }
         if (messageType === "[object Object]") { console.info(message);  }
         if (messageType === "[object Error]" ) { console.error(message); }
+    }
+
+    popupCenter(url, title, w, h)
+    {
+      //Fixes dual-screen position                                most browsers       firefox
+        const dualScreenLeft = window.screenLeft !==  undefined ? window.screenLeft : window.screenX;
+        const dualScreenTop  = window.screenTop  !==  undefined ? window.screenTop  : window.screenY;
+
+        const width  = window.innerWidth  ? window.innerWidth  : document.documentElement.clientWidth  ? document.documentElement.clientWidth  : screen.width;
+        const height = window.innerHeight ? window.innerHeight : document.documentElement.clientHeight ? document.documentElement.clientHeight : screen.height;
+
+        const systemZoom = width / window.screen.availWidth;
+        const left = (width - w) / 2 / systemZoom + dualScreenLeft;
+        const top = (height - h) / 2 / systemZoom + dualScreenTop;
+        const newWindow = window.open
+        (
+            url,
+            title,
+            `
+            scrollbars=yes,
+            width=${w / systemZoom},
+            height=${h / systemZoom},
+            top=${top},
+            left=${left}
+            `
+        );
+
+        if (window.focus) {
+            newWindow.focus();
+        }
     }
 }
