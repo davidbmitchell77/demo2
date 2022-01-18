50e450f412a9c7dac202d608e32f2bcc04252706
David Mitchell <davidbmitchell77@outlook.com>
2022-01-17 21:26:59 -0800

    ...

--- metadatasource/lwc/checkPermissionDemo/checkPermissionDemo.js
+++ metadatasource/lwc/checkPermissionDemo/checkPermissionDemo.js
@@ -1,25 +1,28 @@
 import { LightningElement } from "lwc";
 
 import AssignPermissionSets from "@salesforce/userPermission/AssignPermissionSets";
-import ManageEncyptionKeys  from "@salesforce/userPermission/ManageEncyptionKeys";
+import ManageEncryptionKeys from "@salesforce/userPermission/ManageEncryptionKeys";
 import ModifyAllData        from "@salesforce/userPermission/ModifyAllData";
 import ViewAllData          from "@salesforce/userPermission/ViewAllData";
+import DataExport           from "@salesforce/userPermission/DataExport";
 
 export default class CheckPermissionDemo extends LightningElement
 {
     assignPermissionSets = AssignPermissionSets;
-    manageEncyptionKeys  = ManageEncyptionKeys;
+    manageEncryptionKeys = ManageEncryptionKeys;
     modifyAllData        = ModifyAllData;
     viewAllData          = ViewAllData;
+    dataExport           = DataExport;
 
     get userPermissions()
     {
         let results = new Array();
 
         results.push({ label: "Assign Permission Sets", value: this.assignPermissionSets });
-        results.push({ label: "View All Data", value: this.viewAllData });
-        results.push({ label: "Modify All Data", value: this.modifyAllData });
-        results.push({ label: "Manage Encryption Keys", value: this.manageEncyptionKeys });
+        results.push({ label: "View All Data"         , value: this.viewAllData          });
+        results.push({ label: "Modify All Data"       , value: this.modifyAllData        });
+        results.push({ label: "Manage Encryption Keys", value: this.manageEncryptionKeys });
+        results.push({ label: "Export Data"           , value: this.dataExport           });
 
         return results;
     }
