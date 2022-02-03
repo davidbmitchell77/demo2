fb4b109130829dd26f0238ea38e3a8d0db59e91f
David Mitchell <davidbmitchell77@outlook.com>
2022-02-02 17:01:36 -0800

    chartDemo

--- force-app/main/default/lwc/chartJs/chartJs.js
+++ force-app/main/default/lwc/chartJs/chartJs.js
@@ -1,75 +1,80 @@
-import { LightningElement } from "lwc";
-import { loadScript       } from "lightning/platformResourceLoader";
+import { LightningElement, api } from 'lwc';
+import { loadScript            } from 'lightning/platformResourceLoader';
 
-import chartJs from "@salesforce/resourceUrl/chartJs";
+import chartJs from '@salesforce/resourceUrl/chartJs';
 
-export default class ChartJs extends LightningElement
+export default class Charts extends LightningElement
 {
-    isInitialized = false;
+    @api type;
+    @api chartData;
+    @api chartHeading;
+    @api chartLabels;
+
+    isChartJsInitialized = false;
     chart;
 
     renderedCallback()
     {
-        if (!this.isInitialized)
+        if (!this.isChartJsInitialized)
         {
-            (loadScript(this, (chartJs + "/Chart.js-master/Chart.js"))).then
+            console.log("type: ", this.type);
+            loadScript(this, (chartJs + '/chartJs/Chart.js')).then
             (
                 () => {
                     this.loadCharts();
-                    this.isInitialized = true;
-                    console.log("Chart.js successfully loaded.");
+                    this.isChartJsInitialized = true;
+                    console.log("chartJs loaded succesfully.");
                 }
             ).catch
             (
                 (error) => {
                     console.error(error);
                 }
-            );
+            )
         }
     }
 
     loadCharts()
     {
         window.Chart.platform.disableCSSInjection = true;
-        const canvas = document.createElement("canvas");
-        this.template.querySelector("div.chart").appendChild("canvas");
-        const ctx = canvas.getContext("2d");
-        this.chart = window.Chart(ctx, this.config);
+        const canvas = document.createElement('canvas');
+        this.template.querySelector('div.chart').appendChild(canvas);
+        const ctx = canvas.getContext('2d');
+        this.chart = new window.Chart(ctx, this.config());
     }
 
-    config =
-    ({
-        type: 'bar',
-        data: {
-            labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
-            datasets: [{
-                label: '# of Votes',
-                data: [12, 19, 3, 5, 2, 3],
-                backgroundColor: [
-                    'rgba(255, 99, 132, 0.2)',
-                    'rgba(54, 162, 235, 0.2)',
-                    'rgba(255, 206, 86, 0.2)',
-                    'rgba(75, 192, 192, 0.2)',
-                    'rgba(153, 102, 255, 0.2)',
-                    'rgba(255, 159, 64, 0.2)'
-                ],
-                borderColor: [
-                    'rgba(255, 99, 132, 1)',
-                    'rgba(54, 162, 235, 1)',
-                    'rgba(255, 206, 86, 1)',
-                    'rgba(75, 192, 192, 1)',
-                    'rgba(153, 102, 255, 1)',
-                    'rgba(255, 159, 64, 1)'
-                ],
-                borderWidth: 1
-            }]
-        },
-        options: {
-            scales: {
-                y: {
-                    beginAtZero: true
+    config() {
+        return {
+            type: this.type,
+            data: {
+                labels: ((this.chartLabels) ? this.chartLabels : []),
+                datasets: [{
+                    label: this.chartHeading,
+                    data: ((this.chartData) ? this.chartData : []),
+                    backgroundColor: [
+                        'rgba(255, 99, 132, 0.8)',
+                        'rgba(54, 162, 235, 0.8)',
+                        'rgba(255, 206, 86, 0.8)',
+                        'rgba(75, 192, 192, 0.8)',
+                        'rgba(153, 102, 255, 0.8)',
+                        'rgba(255, 159, 64, 0.8)',
+                        'rgba(30, 204, 148, 0.8)',
+                        'rgba(130, 204, 148, 0.8)'
+
+                    ],
+                    borderWidth: 1
+                }]
+            },
+            options: {
+                responsive: true,
+                legend: {
+                    position: 'right'
+                },
+                animation: {
+                    animateScale: true,
+                    animateRotate: true
                 }
             }
         }
-    });
+    }
 }
