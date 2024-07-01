9c5b21b04b13d8791b7b25cee08d974c540726c1
David B. Mitchell <davidbmitchell77@outlook.com>
2024-07-01 17:24:35 +0000

    .

--- force-app/main/default/lwc/lightningDataTableChild/lightningDataTableChild.js
+++ force-app/main/default/lwc/lightningDataTableChild/lightningDataTableChild.js
@@ -6,15 +6,17 @@ export default class LightningDataTableChild extends LightningElement {
     @api cols;
     @api records;
 
-    sortBy;
-    sortDirection;
-    draftValues;
+    sortBy        = '';
+    sortDirection = '';
+    draftValues   = [];
 
     doSave(event) {
-        if (event) {
-            console.clear();
-            console.info(event.detail);
-        }
+        this.draftValues = [ ...event.detail.draftValues];
+        console.info(this.draftValues);
+        const records = event.detail.draftValues.slice().map((draftValue) => {
+          const fields = Object.assign({}, draftValue);
+          return { fields };
+        });
     }
 
     doSorting(event) {
@@ -37,3 +39,91 @@ export default class LightningDataTableChild extends LightningElement {
         this.records = parseData;
     }
 }
+
+/*
+// apexContactsForAccount.js
+
+import { LightningElement, wire, api } from "lwc";
+import getContacts from "@salesforce/apex/ContactController.getContactList";
+import { refreshApex } from "@salesforce/apex";
+import { updateRecord } from "lightning/uiRecordApi";
+
+import { ShowToastEvent } from "lightning/platformShowToastEvent";
+import FIRSTNAME_FIELD from "@salesforce/schema/Contact.FirstName";
+import LASTNAME_FIELD from "@salesforce/schema/Contact.LastName";
+import TITLE_FIELD from "@salesforce/schema/Contact.Title";
+import PHONE_FIELD from "@salesforce/schema/Contact.Phone";
+import EMAIL_FIELD from "@salesforce/schema/Contact.Email";
+import ID_FIELD from "@salesforce/schema/Contact.Id";
+
+const COLS = [
+  {
+    label: "First Name",
+    fieldName: FIRSTNAME_FIELD.fieldApiName,
+    editable: true,
+  },
+  {
+    label: "Last Name",
+    fieldName: LASTNAME_FIELD.fieldApiName,
+    editable: true,
+  },
+  { label: "Title", fieldName: TITLE_FIELD.fieldApiName, editable: true },
+  {
+    label: "Phone",
+    fieldName: PHONE_FIELD.fieldApiName,
+    type: "phone",
+    editable: true,
+  },
+  {
+    label: "Email",
+    fieldName: EMAIL_FIELD.fieldApiName,
+    type: "email",
+    editable: true,
+  },
+];
+export default class DatatableInlineEditWithUiApi extends LightningElement {
+  @api recordId;
+  columns = COLS;
+  draftValues = [];
+
+  @wire(getContacts, { accId: "$recordId" })
+  contacts;
+
+  async handleSave(event) {
+    // Convert datatable draft values into record objects
+    const records = event.detail.draftValues.slice().map((draftValue) => {
+      const fields = Object.assign({}, draftValue);
+      return { fields };
+    });
+
+    // Clear all datatable draft values
+    this.draftValues = [];
+
+    try {
+      // Update all records in parallel thanks to the UI API
+      const recordUpdatePromises = records.map((record) => updateRecord(record));
+      await Promise.all(recordUpdatePromises);
+
+      // Report success with a toast
+      this.dispatchEvent(
+        new ShowToastEvent({
+          title: "Success",
+          message: "Contacts updated",
+          variant: "success",
+        }),
+      );
+
+      // Display fresh data in the datatable
+      await refreshApex(this.contacts);
+    } catch (error) {
+      this.dispatchEvent(
+        new ShowToastEvent({
+          title: "Error updating or reloading contacts",
+          message: error.body.message,
+          variant: "error",
+        }),
+      );
+    }
+  }
+}
+*/
