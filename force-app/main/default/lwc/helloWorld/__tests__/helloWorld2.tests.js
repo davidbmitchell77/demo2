a3e498d2b4b5a05eb72bebdbee07959953d74108
David B. Mitchell <davidbmitchell77@outlook.com>
2024-07-11 01:13:53 +0000

    .

--- force-app/main/default/lwc/helloWorld/__tests__/helloWorld2.tests.js
+++ force-app/main/default/lwc/helloWorld/__tests__/helloWorld2.tests.js
@@ -1,15 +1,20 @@
 import { createElement } from 'lwc';
 import HelloWorld from 'c/helloWorld';
 
-describe('c-helloWorld', () => {
+describe('c-hello-world', () => {
     afterEach(() => {
         while (document.body.firstChild) {
             document.body.removeChild(document.body.firstChild);
         }
     })
 
-    test('dd', () => {
-        let i = 1;
-        expect(++i).toBe(2);
+    test('foobar', () => {
+        const element = createElement('c-hello-world', {
+            is:HelloWorld
+        });
+        document.body.appendChild(element);
+        const div = element.shadowRoot.querySelectorAll('div')[0];
+        console.log(div.textContent);
+        expect(div.textContent).toBe('"Zero to Hero" is a course of LWC.');
     })
 })
