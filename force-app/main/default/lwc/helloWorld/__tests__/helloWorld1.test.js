9c312c26ba12957b91232335c792a28a3e4bcdb5
David B. Mitchell <davidbmitchell77@outlook.com>
2024-07-16 22:12:55 +0000

    .

---
+++ force-app/main/default/lwc/helloWorld/__tests__/helloWorld1.test.js
@@ -0,0 +1,26 @@
+describe("Test Suite", () => {
+    beforeEach(() => {
+        console.log('Before each running...')
+    })
+
+    afterEach(() => {
+        console.log('After each running...')
+    })
+
+    test("(1 + 2) equals 3", () => {
+        let n = (1 + 2)
+        expect(n).toBe(3)
+    })
+
+    test("(2 + 2) equals 4", () => {
+        let n = (2 + 2)
+        expect(n).toBe(4)
+    })
+
+    it("(5 * 5) equals 25", () => {
+        let n = (5 * 5);
+        expect(n).toBeLessThan(250)
+        expect(n).toBe(25)
+        expect(n).toBeGreaterThan(2.5);
+    })
+});
