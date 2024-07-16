2090e8976419df9ff7d4e70e691b47e79252dc9f
David B. Mitchell <davidbmitchell77@outlook.com>
2024-07-16 22:08:11 +0000

    .

---
+++ force-app/main/default/lwc/helloWorld/__tests__/helloWorld2.test.js
@@ -0,0 +1,17 @@
+import { createElement } from 'lwc';
+import HelloWorld from 'c/helloWorld';
+
+describe('c-hello-world', () => {
+    afterEach(() => {
+        while (document.body.firstChild) {
+            document.body.removeChild(document.body.firstChild);
+        }
+    });
+
+    test('Zero to Hero', () => {
+        const element = createElement('c-hello-world', { is: HelloWorld });
+        document.body.appendChild(element);
+        const div = element.shadowRoot.querySelectorAll('div')[0];
+        expect(div.textContent).toBe('"Zero to Hero" is a course of LWC.');
+    });
+});
