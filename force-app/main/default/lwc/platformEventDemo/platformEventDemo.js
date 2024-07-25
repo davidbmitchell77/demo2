caefb2f0cc3902cd8ed774ce9ec93a1ca8fed48f
David B. Mitchell <davidbmitchell77@outlook.com>
2024-07-25 00:35:56 -0700

    Update platformEventDemo.js

--- force-app/main/default/lwc/platformEventDemo/platformEventDemo.js
+++ force-app/main/default/lwc/platformEventDemo/platformEventDemo.js
@@ -5,49 +5,45 @@ import { showToast              } from 'c/utils';
 
 export default class PlatformEventDemo extends LightningElement {
 
-    channelName   = 'LightningWebComponent__e';
-    subDisabled   = false;
+    channelName   = '/event/LightningWebComponent__e';
+    subDisabled   = true;
     unsubDisabled = true;
     subscription  = {};
 
     handleChange(event) {
         if (event.target.label == 'Channel Name') {
-            this.channelName = event.target.value;
+            this.channelName = event.target.values;
         }
     }
 
     connectedCallback() {
         this.registerErrorListener();
+        this.subDisabled = false;
     }
 
     sub() {
-        const messageCallback = ((response) => {
-            console.info(response);
-        });
-
-        subscribe(this.channelName, -1, messageCallback)
+        subscribe(this.channelName, -1, null)
        .then((response) => {
+            console.info({ ...response });
             this.subscription = { ...response };
             this.toggle();
             showToast(this, 'Success', `You have subscribed to the "${this.channelName}" platform event!`, 'success');
         })
        .catch((error) => {
-            console.error(error);
-            showToast(this, 'Error!', `Error subscribing to the "${this.channelName}" platform event!`, 'error', 'sticky');
+            console.error({ ...error });
+            showToast(this, 'Error!', `Error subscribing to "${this.channelName}" platform event!`, 'error', 'sticky');
        });
     }
 
     unsub() {
-        unsubscribe(this.subscription)
-       .then((response) => {
-            console.warn(response);
-            success = true;
+        unsubscribe(this.subscription, (response) => { console.info({ ...response }); })
+       .then(() => {
             this.toggle();
             showToast(this, 'Unsubscribed', `You have unsubscribed from the "${this.channelName}" platform event!`, 'warning');
         })
        .catch((error) => {
-            console.error(error);
-            showToast(this, 'Error!', `Error unsubscribing from the "${this.channelName}" platform event!`, 'error', 'sticky');
+            console.error({ ...error });
+            showToast(this, 'Error!', `Error unsubscribing from "${this.channelName}" platform event!`, 'error', 'sticky');
         });
     }
 
@@ -58,7 +54,7 @@ export default class PlatformEventDemo extends LightningElement {
 
     registerErrorListener() {
         onError((error) => {
-            console.error(error);
+            console.error({ ...error });
             showToast(this, 'Error!', 'Lightning web component listener failure (PlatformEventDemo).', 'error', 'pester');
         });
     }
