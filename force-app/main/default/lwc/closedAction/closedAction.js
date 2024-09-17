8800a81e6ed1a0c46040b079bf448b2b3688ac66
David B. Mitchell <davidbmitchell77@outlook.com>
2024-09-17 01:45:58 +0000

    .

--- force-app/main/default/lwc/closedAction/closedAction.js
+++ force-app/main/default/lwc/closedAction/closedAction.js
@@ -1,35 +1,28 @@
-import { LightningElement, api } from "lwc";
-import { updateRecord          } from "lightning/uiRecordApi";
-import { ShowToastEvent        } from "lightning/platformShowToastEvent";
+import { LightningElement, api } from 'lwc';
+import { updateRecord          } from 'lightning/uiRecordApi';
+import { ShowToastEvent        } from 'lightning/platformShowToastEvent';
 
-import id_field         from "@salesforce/schema/Opportunity.Id";
-import stage_name_field from "@salesforce/schema/Opportunity.StageName";
+import id_field         from '@salesforce/schema/Opportunity.Id';
+import stage_name_field from '@salesforce/schema/Opportunity.StageName';
 
-export default class ClosedAction extends LightningElement
-{
+export default class ClosedAction extends LightningElement {
     @api recordId;
     @api objectApiName;
 
-    @api invoke()
-    {
+    @api invoke() {
         let fields = {};
         fields[id_field.fieldApiName] = this.recordId;
-        fields[stage_name_field.fieldApiName] = "Closed";
+        fields[stage_name_field.fieldApiName] = 'Closed';
 
         let recordInput = { fields };
-        updateRecord(recordInput).then
-        (
-            () => {
-                this.showToast("Success!", `Opportunity ${this.recordId} was successfully closed.`, "success");
-                console.log("Invoked closedAction for " + this.objectApiName + "[Id=\"" + this.recordId + "\"].");
-            }
-        )
-        .catch
-        (
-            (error) => {
-                this.showToast("Error!", error.message, "error");
-            }
-        );
+        updateRecord(recordInput)
+       .then(() => {
+            this.showToast('Success!', `Opportunity ${this.recordId} was successfully closed.`, 'success');
+            console.info('Invoked closedAction for ' + this.objectApiName + '[Id=\'' + this.recordId + '\'].');
+        })
+       .catch((error) => {
+            this.showToast('Error!', error.message, 'error');
+        });
     }
 
     showToast(title, message, variant) {
