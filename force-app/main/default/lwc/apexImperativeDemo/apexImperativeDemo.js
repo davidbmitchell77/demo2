44a497ef939dce3ab100b3ed7259f5e7085c07bf
David B. Mitchell <davidbmitchell77@outlook.com>
2024-09-17 01:21:09 +0000

    .

--- force-app/main/default/lwc/apexImperativeDemo/apexImperativeDemo.js
+++ force-app/main/default/lwc/apexImperativeDemo/apexImperativeDemo.js
@@ -1,23 +1,16 @@
-import { LightningElement } from "lwc";
+import { LightningElement } from 'lwc';
 
-import getAccountList from "@salesforce/apex/AccountController.getAccountList";
+import getAccountList from '@salesforce/apex/AccountController.getAccountList';
 
-export default class ApexImperativeDemo extends LightningElement
-{
+export default class ApexImperativeDemo extends LightningElement {
     accounts = [];
 
-    clickHandler()
-    {
-        getAccountList().then (
-            result =>
-            {
-                this.accounts = result;
-                console.info(this.accounts);
-            }
-        ).catch(
-            error => {
-                console.error(error);
-            }
-        );
+    clickHandler() {
+        getAccountList().then ((result) => {
+            this.accounts = result;
+            console.info(this.accounts);
+        }).catch((error) => {
+            console.error(error);
+        });
     }
 }
