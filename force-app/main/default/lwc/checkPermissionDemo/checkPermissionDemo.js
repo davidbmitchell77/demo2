b442d7ccb0c8c982cc5760f1da6d5085c4ccc5e7
David B. Mitchell <davidbmitchell77@outlook.com>
2024-09-17 01:42:34 +0000

    .

--- force-app/main/default/lwc/checkPermissionDemo/checkPermissionDemo.js
+++ force-app/main/default/lwc/checkPermissionDemo/checkPermissionDemo.js
@@ -1,34 +1,29 @@
-import { LightningElement } from "lwc";
+import { LightningElement } from 'lwc';
 
-import AssignPermissionSets from "@salesforce/userPermission/AssignPermissionSets";
-import ManageEncryptionKeys from "@salesforce/userPermission/ManageEncryptionKeys";
-import ModifyAllData        from "@salesforce/userPermission/ModifyAllData";
-import ViewAllData          from "@salesforce/userPermission/ViewAllData";
-import DataExport           from "@salesforce/userPermission/DataExport";
+import AssignPermissionSets from '@salesforce/userPermission/AssignPermissionSets';
+import ManageEncryptionKeys from '@salesforce/userPermission/ManageEncryptionKeys';
+import ModifyAllData        from '@salesforce/userPermission/ModifyAllData';
+import ViewAllData          from '@salesforce/userPermission/ViewAllData';
+import DataExport           from '@salesforce/userPermission/DataExport';
 
-export default class CheckPermissionDemo extends LightningElement
-{
+export default class CheckPermissionDemo extends LightningElement {
     assignPermissionSets = AssignPermissionSets;
     manageEncryptionKeys = ManageEncryptionKeys;
     modifyAllData        = ModifyAllData;
     viewAllData          = ViewAllData;
     dataExport           = DataExport;
 
-    get userPermissions()
-    {
+    get userPermissions() {
         let results = new Array();
-
-        results.push({ label: "Assign Permission Sets", value: this.assignPermissionSets });
-        results.push({ label: "View All Data"         , value: this.viewAllData          });
-        results.push({ label: "Modify All Data"       , value: this.modifyAllData        });
-        results.push({ label: "Manage Encryption Keys", value: this.manageEncryptionKeys });
-        results.push({ label: "Export Data"           , value: this.dataExport           });
-
+        results.push({ label: 'Assign Permission Sets', value: this.assignPermissionSets });
+        results.push({ label: 'View All Data'         , value: this.viewAllData          });
+        results.push({ label: 'Modify All Data'       , value: this.modifyAllData        });
+        results.push({ label: 'Manage Encryption Keys', value: this.manageEncryptionKeys });
+        results.push({ label: 'Export Data'           , value: this.dataExport           });
         return results;
     }
 
-    get checkedValues()
-    {
+    get checkedValues() {
         let results = new Array();
         results.push(true);
         return results;
