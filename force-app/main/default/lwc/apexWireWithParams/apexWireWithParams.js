cede3c4a679be4a8d1f2632b9289810c11b2ee62
David B. Mitchell <davidbmitchell77@outlook.com>
2024-09-17 01:32:17 +0000

    .

--- force-app/main/default/lwc/apexWireWithParams/apexWireWithParams.js
+++ force-app/main/default/lwc/apexWireWithParams/apexWireWithParams.js
@@ -1,29 +1,25 @@
-import { LightningElement, wire } from "lwc";
+import { LightningElement, wire } from 'lwc';
 
-import getAccountListByType from "@salesforce/apex/AccountController.getAccountListByType";
+import getAccountListByType from '@salesforce/apex/AccountController.getAccountListByType';
 
-export default class ApexWireWithParams extends LightningElement
-{
+export default class ApexWireWithParams extends LightningElement {
     selectedType = null;
 
-    @wire(getAccountListByType, { type: "$selectedType" })
+    @wire(getAccountListByType, { type: '$selectedType' })
     filteredAccounts;
 
     typeHandler(event) {
         this.selectedType = event.target.value;
     }
 
-    get accountTypeOptions()
-    {
+    get accountTypeOptions() {
         let options = [];
-
-        options.push({ label: "--None--"             , value: null                   });
-        options.push({ label: "Customer - Channel"   , value: "Customer - Channel"   });
-        options.push({ label: "Customer - Direct"    , value: "Customer - Direct"    });
-        options.push({ label: "Installation Partner" , value: "Installation Partner" });
-        options.push({ label: "Prospect"             , value: "Prospect"             });
-        options.push({ label: "Technology Partner"   , value: "Technology Partner"   });
-
+        options.push({ label: '--None--'             , value: null                   });
+        options.push({ label: 'Customer - Channel'   , value: 'Customer - Channel'   });
+        options.push({ label: 'Customer - Direct'    , value: 'Customer - Direct'    });
+        options.push({ label: 'Installation Partner' , value: 'Installation Partner' });
+        options.push({ label: 'Prospect'             , value: 'Prospect'             });
+        options.push({ label: 'Technology Partner'   , value: 'Technology Partner'   });
         return options;
     }
 }
