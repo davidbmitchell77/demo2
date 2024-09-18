14e76c48a3cdb20e935e673f1bf3f5ab748f478d
David B. Mitchell <davidbmitchell77@outlook.com>
2024-09-18 23:42:01 +0000

    .

--- force-app/main/default/lwc/fetch/fetch.js
+++ force-app/main/default/lwc/fetch/fetch.js
@@ -8,7 +8,6 @@ export default class Fetch extends LightningElement {
     connectedCallback() {
         fetch(this.url)
        .then((response) => {
-            console.clear();
             console.info(response);
             if (response.ok) {
               showToast(this, `${response.status}: ${httpStatusText[response.status]}`, `(${response.url})`, 'success');
@@ -22,6 +21,7 @@ export default class Fetch extends LightningElement {
             this.users = data;
         })
        .catch((error) => {
+            console.error(error);
             showToast(this, 'Error!', `${error.message} (${this.url})`, 'error', 'pester');
         });
     }
