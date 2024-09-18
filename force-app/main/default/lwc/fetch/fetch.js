1d0169a742272d2e19e59f3ce272a55f6f3702d9
David B. Mitchell <davidbmitchell77@outlook.com>
2024-09-18 20:28:59 +0000

    .

--- force-app/main/default/lwc/fetch/fetch.js
+++ force-app/main/default/lwc/fetch/fetch.js
@@ -1,24 +1,32 @@
 import { LightningElement } from 'lwc';
 import { showToast        } from 'c/utils'
 
+const HTTP_STATUS = {
+  200: 'OK',
+  404: 'Page not found.'
+}
+
 export default class Fetch extends LightningElement {
     users;
 
-    url = 'https://jsonplaceholder.typicode.com/users/';  // YOU MUST ADD THIS URL TO: Setup > Security > Trusted URLs
+    url = 'https://jsonplaceholder.typicode.com/userssssssss/';  // YOU MUST ADD THIS URL TO: Setup > Security > Trusted URLs
 
     connectedCallback() {
         fetch(this.url)
           .then((response) => {
+            console.clear();
+            console.info(response);
+            if (!response.ok) {
+              showToast(this, `${response.status}`, `${HTTP_STATUS[response.status]} (${response.url})`, 'error', 'pester');
+            }
             return response.json();
           })
           .then((data) => {
-            console.clear();
             console.info(data);
-            this.users = [ ...data ];
+            this.users = data;
           })
           .catch((error) => {
-            console.clear();
-            showToast(this, `Error!`, error.message, 'error', 'pester');
+            showToast(this, 'Error!', `${error.message} (${this.url})`, 'error', 'pester');
           });
     }
 }
