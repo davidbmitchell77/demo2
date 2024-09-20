ca3e590295fbd888f8722b0d3374f9fca415cfe7
David B. Mitchell <davidbmitchell77@outlook.com>
2024-09-20 23:02:19 +0000

    .

--- force-app/main/default/aura/lmsAuraComponent/lmsAuraComponentController.js
+++ force-app/main/default/aura/lmsAuraComponent/lmsAuraComponentController.js
@@ -1,8 +1,8 @@
 ({
     handleMessage : function(component, message)
     {
-        if (message != null && message.getParam("lmsData") != null) {
-            component.set("v.messageReceived", message.getParam("lmsData").value);
+        if (message != null && message.getParam("message") != null) {
+            component.set("v.messageReceived", message.getParam("message").value);
         }
     },
     inputHandler : function(component, event)
@@ -14,8 +14,23 @@
         let msg = component.get("v.messageValue");
         let message =
         {
-            lmsData: {
+            message: {
                 value: msg
+            },
+            senderId: {
+                value: 'dddd'
+            },
+            senderAlias: {
+                value: 'dmitc'
+            },
+            senderEmail: {
+                value: 'davidbmitchel77@outlook.com'
+            },
+            senderUsername: {
+                value: 'davidbmitchel77@outlook.com.lightning'
+            },
+            timestamp: {
+                 value: new Date()
             }
         };
         component.find("SampleMessageChannel").publish(message);
