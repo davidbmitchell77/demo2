68df7b98024b8d0862d748698ec3fa1860c7dc65
David B. Mitchell <davidbmitchell77@outlook.com>
2024-09-25 04:57:54 +0000

    .

--- force-app/main/default/lwc/wireDemoUserDetailAsAFunction/wireDemoUserDetailAsAFunction.js
+++ force-app/main/default/lwc/wireDemoUserDetailAsAFunction/wireDemoUserDetailAsAFunction.js
@@ -1,36 +1,21 @@
 import { LightningElement, wire } from 'lwc';
 import { getRecord              } from 'lightning/uiRecordApi';
 
-import Id from '@salesforce/user/Id';
+import USER_ID from '@salesforce/user/Id';
 
-export default class WireDemoUserDetail extends LightningElement
-{
-    userId = Id;
+export default class WireDemoUserDetail extends LightningElement {
+    userId = USER_ID;
     userDetails;
-    errMsg = "";
+    errMsg = '';
 
-    @wire
-    (
-        getRecord,
-        {
-            recordId: "$userId",
-            fields:
-            [
-                "User.Name",
-                "User.Title",
-                "User.Email",
-                "User.Phone"
-            ]
+    @wire(getRecord, { recordId: "$userId", fields: [ 'User.Name', 'User.Title', 'User.Email', 'User.Phone' ] })
+    userDetailHandler({ data, error }) {
+        if (data) {
+            this.userDetails = data.fields;
         }
-    )
-    userDetailHandler(response)
-    {
-        if (response.data) {
-            this.userDetails = response.data.fields;
-        }
-        if (response.error) {
-            this.errMsg = response.error.body.message;
+        if (error) {
+            this.errMsg = error.body.message;
             console.error(this.errMsg);
         }
-    };
+    }
 }
