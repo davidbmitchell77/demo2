c735b3d6691ea11fe959900c9a9d4cfbc7ddde3e
David B. Mitchell <davidbmitchell77@outlook.com>
2024-09-25 05:04:45 +0000

    g

--- force-app/main/default/lwc/userInformation/userInformation.js
+++ force-app/main/default/lwc/userInformation/userInformation.js
@@ -2,8 +2,7 @@ import { LightningElement } from 'lwc';
 import user_id from '@salesforce/user/Id';
 import is_guest from '@salesforce/user/isGuest';
 
-export default class UserInformation extends LightningElement
-{
+export default class UserInformation extends LightningElement {
     userId = user_id;
     isGuest = is_guest;
 
